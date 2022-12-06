e46f06231499e7f611915e8cddd928c48f4a1033
zelitosaide <zelitosaide83@gmail.com>
2022-12-06 23:02:30 +0200

    Recipes

--- src/routes/recipe.js
+++ src/routes/recipe.js
@@ -1,4 +1,6 @@
+import { useContext, useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
+import { CartContext } from "../provider";
 
 const baseURL = "http://localhost:5000/";
 
@@ -19,6 +21,91 @@ export async function loader({ params }) {
 
 export function Recipe() {
   const { ingredients = [], ...recipe } = useLoaderData();
+  const {
+    cart: { items },
+    add,
+    remove,
+    update,
+  } = useContext(CartContext);
 
-  console.log(recipe);
+  const recipeFoundInCart = items.find(function (item) {
+    return item._id === recipe._id;
+  }) || { ...recipe, ingredients };
+
+  useEffect(function () {
+    if (recipeFoundInCart && !recipeFoundInCart.ingredients) {
+      update({ _id: recipe._id, ingredients });
+    }
+  }, []);
+
+  return (
+    <div>
+      <div
+        style={{
+          marginTop: 70,
+          padding: "10px 20px",
+        }}
+      >
+        {recipeFoundInCart && (
+          <>
+            <h3 style={{ color: "#333" }}>{recipeFoundInCart.name}</h3>
+
+            <p>
+              <b>Imagem da Receita</b>: {recipeFoundInCart.imageUrl}
+            </p>
+
+            <p>
+              <b>Descricao:</b> {recipeFoundInCart.description}
+            </p>
+
+            {recipeFoundInCart.ingredients && (
+              <>
+                <p>
+                  <b>Lista dos Produtos:</b>
+                </p>
+                <ul>
+                  {recipeFoundInCart.ingredients.map(function (ingredient) {
+                    const ingredientFoundInCart = items.find(function (item) {
+                      return item._id === ingredient._id;
+                    });
+
+                    return (
+                      <li key={ingredient._id}>
+                        {ingredient.name}
+
+                        {!!ingredient?.flag?.createdByAdmin && (
+                          <>
+                            {!!ingredientFoundInCart && (
+                              <button
+                                onClick={function () {
+                                  remove(ingredient);
+                                }}
+                              >
+                                -
+                              </button>
+                            )}
+                            <span>
+                              {!!ingredientFoundInCart &&
+                                ingredientFoundInCart.quantity}
+                            </span>
+                            <button
+                              onClick={function () {
+                                add(ingredient);
+                              }}
+                            >
+                              +
+                            </button>
+                          </>
+                        )}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </>
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
