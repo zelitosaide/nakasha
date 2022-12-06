1300a26ced7000138223a3d53f798c09d3e9cc10
zelitosaide <zelitosaide83@gmail.com>
2022-12-06 18:38:27 +0200

    Recipes

---
+++ src/routes/recipes-horizontal-lazy-load.js
@@ -0,0 +1,97 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { FixedSizeList as List } from "react-window";
+import InfiniteLoader from "react-window-infinite-loader";
+
+import { CartContext } from "../provider";
+
+export function RecipesHorizontalLazyLoad({
+  hasNextPage,
+  isNextPageLoading,
+  items,
+  loadNextPage,
+}) {
+  const itemCount = hasNextPage ? items.length + 3 : items.length;
+
+  const loadMoreItems = isNextPageLoading ? () => {} : loadNextPage;
+
+  const isItemLoaded = (index) => !hasNextPage || index < items.length;
+
+  const { cart, add, remove } = useContext(CartContext);
+
+  function Item({ index, style }) {
+    let content;
+
+    if (!isItemLoaded(index)) {
+      content = "Loading...";
+    } else {
+      const boxFoundInCart = cart.items.find(function (item) {
+        return item._id === items[index]._id;
+      });
+
+      content = (
+        <>
+          <p style={{ marginBottom: 0 }}>
+            {items[index].name}
+            <br />
+            {boxFoundInCart && (
+              <button
+                onClick={function () {
+                  remove(items[index]);
+                }}
+              >
+                -
+              </button>
+            )}
+            <span>{boxFoundInCart && boxFoundInCart.quantity}</span>
+            <button
+              onClick={function () {
+                add(items[index]);
+              }}
+            >
+              +
+            </button>
+          </p>
+          <div>
+            <Link to={`${items[index].category}/${items[index]._id}`}>
+              See Details
+            </Link>
+          </div>
+        </>
+      );
+    }
+
+    return (
+      <div
+        className={index % 2 ? "ListItemOdd" : "ListItemEven"}
+        style={style}
+      >
+        {content}
+      </div>
+    );
+  }
+
+  return (
+    <InfiniteLoader
+      isItemLoaded={isItemLoaded}
+      itemCount={itemCount}
+      loadMoreItems={loadMoreItems}
+    >
+      {({ onItemsRendered, ref }) => (
+        <List
+          className="List"
+          height={95}
+          itemCount={itemCount}
+          itemSize={120}
+          onItemsRendered={onItemsRendered}
+          ref={ref}
+          width={300}
+          layout="horizontal"
+          style={{ width: "100%" }}
+        >
+          {Item}
+        </List>
+      )}
+    </InfiniteLoader>
+  );
+}
--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -1,3 +1,142 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import vegetais from "../assets/images/vegetais.png";
+import frutas from "../assets/images/frutas.png";
+import mercearia from "../assets/images/mercearia.png";
+import { RecipesHorizontalLazyLoad } from "./recipes-horizontal-lazy-load";
+
 export function Recipes() {
-  return <h3 style={{ padding: 10 }}>receitas</h3>;
+  const [state, setState] = useState({
+    hasNextPage: true,
+    isNextPageLoading: false,
+    items: [],
+  });
+  const [page, setPage] = useState(1);
+
+  const LIMIT = 5;
+  const baseUrl = "http://localhost:5000/";
+
+  async function loadNextPage() {
+    return fetch(
+      `${baseUrl}boxes?limit=${LIMIT}&page=${page}&category=hortalica`
+    )
+      .then(function (response) {
+        setState(function (prevState) {
+          return {
+            ...prevState,
+            isNextPageLoading: true,
+          };
+        });
+        return response.json();
+      })
+      .then(function ({ items, pageInfo: { totalResults } }) {
+        setPage(function (prevPage) {
+          return prevPage + 1;
+        });
+
+        setState(function (prevState) {
+          return {
+            ...prevState,
+            hasNextPage: prevState.items.length < totalResults,
+            isNextPageLoading: false,
+            items: [...prevState.items].concat(items),
+          };
+        });
+      });
+  }
+
+  return (
+    <div>
+      {/*  */}
+      <div
+        style={{
+          marginTop: 70,
+          padding: "10px 20px",
+        }}
+      >
+        <h5 style={{ color: "#333" }}>Categorias de Receitas</h5>
+        <div
+          className="cat"
+          style={{ display: "flex", gap: 10 }}
+        >
+          <div style={{ border: "1px solid #d9dddd", padding: 5 }}>
+            <img
+              src={vegetais}
+              alt="vegetais"
+              style={{ width: 90 }}
+            />
+            <div style={{ textAlign: "center" }}>Vegetais</div>
+          </div>
+          <div style={{ border: "1px solid #d9dddd", padding: 5 }}>
+            <img
+              src={frutas}
+              alt="frutas"
+              style={{ width: 80 }}
+            />
+            <div style={{ textAlign: "center" }}>Frutas</div>
+          </div>
+          <div style={{ border: "1px solid #d9dddd", padding: 5 }}>
+            <img
+              src={mercearia}
+              alt="mercearia"
+              style={{ width: 80 }}
+            />
+            <div style={{ textAlign: "center", paddingBottom: 10 }}>
+              Mercearia
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* banner */}
+      <div
+        style={{
+          height: 180,
+          marginTop: 10,
+        }}
+      >
+        <div
+          style={{
+            height: 180,
+            marginLeft: 20,
+            marginRight: 20,
+            background: "#f8f8f0",
+            flexGrow: 1,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            border: "1px solid #d9dddd",
+          }}
+        >
+          <h5>Banner</h5>
+        </div>
+      </div>
+
+      {/* Lazy load */}
+      <div id="lazy-load">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            flexDirection: "row",
+          }}
+        >
+          <h5 style={{ marginLeft: 20 }}>Receitas da epoca</h5>
+          <Link to="hortalica">
+            <span style={{ paddingRight: 20 }}>Ver todos</span>
+          </Link>
+        </div>
+        <div style={{ marginLeft: 20, marginRight: 20 }}>
+          <RecipesHorizontalLazyLoad
+            hasNextPage={state.hasNextPage}
+            isNextPageLoading={state.isNextPageLoading}
+            items={state.items}
+            loadNextPage={loadNextPage}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
