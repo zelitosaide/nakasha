2cd51ba2c4a082f78fede72f9a293a5e873fdce2
zelitosaide <zelitosaide83@gmail.com>
2022-12-16 21:51:48 +0200

    create, list and update

--- src/routes/products/product-list.js
+++ src/routes/products/product-list.js
@@ -1,2 +1,9 @@
 export async function loader() {}
-export function ProductList() {}
+
+export function ProductList() {
+  return (
+    <div style={{ padding: "100px 0" }}>
+      <h4>Product List</h4>
+    </div>
+  );
+}
