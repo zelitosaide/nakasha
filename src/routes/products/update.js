14bc754cfb72bf628075c9bd00d2c6feb54ed340
zelitosaide <zelitosaide83@gmail.com>
2022-12-16 22:10:56 +0200

    create, list and update

--- src/routes/products/update.js
+++ src/routes/products/update.js
@@ -1,1 +1,12 @@
-export function UpdateProduct() {}
+import { useLoaderData } from "react-router-dom";
+
+export function UpdateProduct() {
+  const product = useLoaderData();
+  console.log(product);
+
+  return (
+    <div style={{ padding: "100px 0" }}>
+      <h4>Edit Product</h4>
+    </div>
+  );
+}
