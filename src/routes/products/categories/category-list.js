6fcd90addf0c9f157a0451caeb6b6f8b74c452de
zelitosaide <zelitosaide83@gmail.com>
2022-12-18 17:10:41 +0200

    product categories

---
+++ src/routes/products/categories/category-list.js
@@ -0,0 +1,14 @@
+import { useLoaderData } from "react-router-dom";
+
+import { baseUrl } from "../../../api";
+
+export async function loader() {
+  const response = await fetch(baseUrl + "/productCategories");
+  const categories = await response.json();
+  return categories;
+}
+
+export function CategoryList() {
+  const categories = useLoaderData();
+  console.log(categories);
+}
