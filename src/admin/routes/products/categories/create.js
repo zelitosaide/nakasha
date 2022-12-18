5fd8b8ea60237d1c053d86cb7e4a1c9f7fb468fa
zelitosaide <zelitosaide83@gmail.com>
2022-12-18 21:17:31 +0200

    product categories

--- src/admin/routes/products/categories/create.js
+++ src/admin/routes/products/categories/create.js
@@ -1,3 +1,59 @@
+import { Form, useNavigate } from "react-router-dom";
+
 export async function action() {}
 
-export function CreateProductCategory() {}
+export function CreateProductCategory() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h4>Create Product Category</h4>
+      <Form
+        method="post"
+        encType="multipart/form-data"
+      >
+        <p>
+          <label htmlFor="category-name">Category Name: </label>
+          <input
+            id="category-name"
+            aria-label="Category Name"
+            name="name"
+            type="text"
+          />
+        </p>
+        <p>
+          <label htmlFor="category-description">Category Description: </label>
+          <textarea
+            id="category-description"
+            aria-label="Category Description"
+            name="description"
+          />
+        </p>
+        <p>
+          <label htmlFor="category-image">Category Image: </label>
+          <input
+            id="category-image"
+            aria-label="Category Image"
+            name="image"
+            type="file"
+          />
+        </p>
+        <p>
+          <button
+            type="submit"
+            style={{ marginRight: 10 }}
+          >
+            Save
+          </button>
+          <button
+            type="button"
+            onClick={function () {
+              navigate("/dashboard/products/categories");
+            }}
+          >
+            Cancel
+          </button>
+        </p>
+      </Form>
+    </div>
+  );
+}
