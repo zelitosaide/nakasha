87063f2571784d2fe0577d62c60805c09852adc6
zelitosaide <zelitosaide83@gmail.com>
2023-01-10 18:35:23 +0200

    Nakasha MVP

--- src/client/routes/cart/cart.js
+++ src/client/routes/cart/cart.js
@@ -11,18 +11,41 @@ export function Cart() {
 
   return (
     <div>
-      {}
-      <p
+      <h1
         style={{
           fontSize: 12,
           fontWeight: 700,
-          // paddingBottom: 12,
           color: "#444",
           padding: "0 20px",
+          margin: 0,
         }}
       >
         Carinho
-      </p>
+      </h1>
+      {products.length && (
+        <div style={{ padding: "0 20px" }}>
+          <h2 style={{ fontSize: 12, color: "#444" }}>Productos</h2>
+          <ul style={{ margin: 0, padding: 0, listStyle: "none" }}>
+            {products.map(function (product) {
+              return (
+                <li key={product._id}>
+                  <div>
+                    <img
+                      src={product.imageUrl}
+                      alt={product.name}
+                    />
+                    <div>
+                      <span>{product.name}</span>
+                      <span>{product.name}</span>
+                      <span>{product.price} MT</span>
+                    </div>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
